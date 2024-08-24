27d4bfea4327380a6bb8327520ec6ae4372f04bc
ayush-kr05 <ayushkumar.barbighaiya@gmail.com>
2024-08-25 01:22:29 +0530

    Intial Setup For Vue App

---
+++ vue-next-tick/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Hello From Vue Tick App</h1>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+body {
+  background-color: rgb(66, 66, 66);
+  color: aliceblue;
+}
+</style>
